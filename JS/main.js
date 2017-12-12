3752eff6633d0dbcdcb87b8023d8a46fb3bd37bb
Brocier <jschoe04@gmail.com>
2017-12-12 16:18:18 -0500

    Successfully appended the letter A

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,8 @@
 
 $(() => {
     console.log(`let's guess!`)
+    $('.lettersRemaining').append('<p>A</p>');
+
+    /* font-weight thin if used and bold if not */
+    /* Use the scale-in and scale-out classes to make the letters appear and disappear. */
 });
