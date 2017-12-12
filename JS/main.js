76e9edb7a804faca1355e02eabb2669a572bc102
Brocier <jschoe04@gmail.com>
2017-12-12 16:24:09 -0500

    Merge to master test

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@
 $(() => {
     console.log(`let's guess!`)
     $('.lettersRemaining').append('<p>A</p>');
-
+    // test to see if I can merge to master
     /* font-weight thin if used and bold if not */
     /* Use the scale-in and scale-out classes to make the letters appear and disappear. */
 });
