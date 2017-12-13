2396abd077b871be919b553e9ba8b14394246ea5
Brocier <jschoe04@gmail.com>
2017-12-12 20:19:00 -0500

    Got my letters and guessWord to append to the approriate divs

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,12 @@
 
 $(() => {
     console.log(`let's guess!`)
-    $('.lettersRemaining').append('<p>A</p>');
+    const guessWord = `fhqwgads`
+    const letters = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`]
+    console.log(letters)
+    $('.lettersRemaining').append(letters);
+    $('.guessingArea').append(guessWord)
+    $('.lettersUsed').append(letters);
     // test to see if I can merge to master
     /* font-weight thin if used and bold if not */
     /* Use the scale-in and scale-out classes to make the letters appear and disappear. */
