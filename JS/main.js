0193584f23d4cb67b637b405fbc33654fd75551c
Brocier <jschoe04@gmail.com>
2017-12-13 12:48:52 -0500

    Added guessedLetter as an argument in the letterCheck function

--- JS/main.js
+++ JS/main.js
@@ -3,24 +3,33 @@ $(() => {
     console.log(`let's guess!`)
     const guessWord = `fhqwgads`
     const letters = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`]
-    // console.log(letters)
 
     //split up the answer into seperate letters
     const guessSplit = guessWord.split("");
     console.log(guessSplit)
 
-    //make a click event for letter
-    $(`.bold`).on(`click`, (event) => {
+    //make a click event that returns the letter clicked
+    const guessedLetter = $(`.bold`).on(`click`, (event) => {
         const letterClicked = event.target.textContent;
         console.log(letterClicked);
         return letterClicked;
     })
-    //
-    // $('.lettersRemaining').append();
+
+    // check to see if the letter clicked is in the answer
+    // 
+    const letterCheck = (guessedLetter) => {
+        if (guessedLetter === guessSplit[0]) {
+            return true
+            console.log(`You hit A`)
+        }
+    }
+    letterCheck(guessedLetter);
+
+
+
     $('.answer').append(guessWord);
     // $('.lettersUsed').append(letters);
 
-    // check to see if the letter clicked is in the answer
 
     // test to see if I can merge to master
     /* font-weight thin if used and bold if not */
