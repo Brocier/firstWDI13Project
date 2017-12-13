ae31e8a6318cb6ca4fad2304a1b7c2f9aa30581c
Brocier <jschoe04@gmail.com>
2017-12-13 10:17:08 -0500

    Wrote out pseudocode for putting the letters in boxes

--- JS/main.js
+++ JS/main.js
@@ -4,12 +4,16 @@ $(() => {
     const guessWord = `fhqwgads`
     const letters = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`]
     // console.log(letters)
-    //create a div that holds the letters
-    //put the letters in that container
-    //put that container on the gameboard
-    // $('.lettersRemaining').append(letters);
-    // $('.guessingArea').append(guessWord);
+
+    //make a click event for letter
+    //use event.target to select the letter I clicked
+    //make what letter I clicked console.log
+    //
+    // $('.lettersRemaining').append();
+    $('.answer').append(guessWord);
     // $('.lettersUsed').append(letters);
+
+    // check to see if the letter clicked is in the answer
 
     // test to see if I can merge to master
     /* font-weight thin if used and bold if not */
