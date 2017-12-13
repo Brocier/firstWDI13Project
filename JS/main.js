c2450c18d9b807813aa21a8a5c4f64c8959b6c21
Brocier <jschoe04@gmail.com>
2017-12-13 09:52:11 -0500

    Put div's for each letter in my html

--- JS/main.js
+++ JS/main.js
@@ -3,10 +3,14 @@ $(() => {
     console.log(`let's guess!`)
     const guessWord = `fhqwgads`
     const letters = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`]
-    console.log(letters)
-    $('.lettersRemaining').append(letters);
-    $('.guessingArea').append(guessWord)
-    $('.lettersUsed').append(letters);
+    // console.log(letters)
+    //create a div that holds the letters
+    //put the letters in that container
+    //put that container on the gameboard
+    // $('.lettersRemaining').append(letters);
+    // $('.guessingArea').append(guessWord);
+    // $('.lettersUsed').append(letters);
+
     // test to see if I can merge to master
     /* font-weight thin if used and bold if not */
     /* Use the scale-in and scale-out classes to make the letters appear and disappear. */
