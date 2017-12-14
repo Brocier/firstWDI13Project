c128336b193976072837778bcd7247cfe9898d60
Brocier <jschoe04@gmail.com>
2017-12-14 09:46:52 -0500

    final commit for the MVP

--- JS/main.js
+++ JS/main.js
@@ -7,9 +7,8 @@ $(() => {
     const guessSplit = guessWord.split("");
     //map the split letters to divs with the class of 'hidden answer'
     const guessFinal = guessSplit.map((letter) => {
-        return $('.answer-container').append(`<span class="hidden answer">${letter}</span>`)
+        return $('.answer-container').append(`<span class="hidden answer ${letter}">${letter}</span>`)
     })
-
     //make a click event that returns the letter clicked
     const guessedLetter = $(`.letter`).on(`click`, (event) => {
         const letterClicked = event.target.textContent;
@@ -17,13 +16,11 @@ $(() => {
         if (guessSplit.indexOf(letterClicked) > -1) {
             $(event.target).css('font-weight', 'bold')
             $(event.target).off()
-
+            $(`.${letterClicked}`).removeClass('hidden')
             // //how I'm getting the letter that matched in the answer.
             const matchingLetterIndex = guessSplit.indexOf(letterClicked)
             console.log('matchingLetterIndex', matchingLetterIndex)
             console.log("guessSplit", guessSplit[matchingLetterIndex]);
-
-            // $('span').removeClass('hidden')
         }
         else {
             $(event.target).addClass('wrong');
@@ -31,8 +28,4 @@ $(() => {
         }
         return letterClicked;
     })
-
-    // test to see if I can merge to master
-    /* font-weight thin if used and bold if not */
-    /* Use the scale-in and scale-out classes to make the letters appear and disappear. */
 });
